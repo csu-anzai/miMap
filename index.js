28c7508ae3e4b24fbc0c5f032bfae1ce41fde51f
Kitorosano <rosanoesteban2@gmail.com>
2019-08-27 23:07:34 -0300

    removing public directory

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Starting server at ${port}`));
-app.use(express.static('public'));
+app.use(express.static('/'));
 app.use(express.json({ limit: '1mb' }));
 
 const database = new Datastore('database.db');
