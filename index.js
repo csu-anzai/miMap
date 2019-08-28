1b5e942c5a780abcab8078fd654489e99fd20936
Kitorosano <rosanoesteban2@gmail.com>
2019-08-27 22:00:00 -0300

    updating index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const Datastore = require('nedb');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Starting server at ${port}`));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    });
+})
